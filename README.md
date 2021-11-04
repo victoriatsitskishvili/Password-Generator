82a323e414ad835473ba648c86eeae37b4f15c5c
victoriatsitskishvili <victoria.tsitskishvili@gmail.com>
2021-11-04 19:07:15 -0400

    adding special characters, numeric and letters to password

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Password Generator Starter Code
+1. Including lowercase, uppercase, numeric, and/or special characters as an Array
+2. To include lowercase - use a var lowercasecharacters
+3. To include uppercase - use a var uppercasecharacters
+4. To include numeric - use a var numericcharacters
+5. To include special characters - use a var specialcharacters
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,105 @@
 // Assignment code here
+//To include lowercase characters
+var lowercasecharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+//To include uppercase characters
+var uppercasecharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+//To include numeric characters
+var numericcharacters = [
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+];
+
+//To include special characters
+var specialcharacters = [
+  '',
+  '!',
+  '”',	
+  '#',	
+  '$',	
+  '%',	
+  '&',	
+  '’',	
+  '(',	
+  ')',	
+  '*',	
+  '+',	
+  ',',	
+  '-',	
+  '.',	
+  '/',	
+  ':',	
+  ';',	
+  '<',	
+  '=',	
+  '>',	
+  '?',	
+  '@',	
+  '[',	
+  '\',	
+  ']',	
+  '^',	
+  '_',	
+  '`',	
+  '{',	
+  '|',	
+  '}',	
+  '~',	
+];
 
 
 // Get references to the #generate element
@@ -8,9 +109,9 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
+  
 }
 
 // Add event listener to generate button
